c1b5f6d8cd3356837848eafb60f6a06fb2481a81
Naimul <mdnaimul2090@gmail.com>
2023-06-14 23:32:03 +0600

    getsetrock frontend setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="">
+    <div class="">
+      <header class="sticky top-0 py-5 bg-transparent">
+        <div class="flex items-center justify-between" id="navbar">
+          <div>
+            <div class="">
+              <img src="./assets/image/logo.png" alt="Site Logo" />
+            </div>
+          </div>
+          <div class="flex items-start">
+            <nav class="text-white flex flex-shrink-0 space-x-3">
+              <router-link to="/">Home</router-link>
+              <router-link to="/about">About Us</router-link>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </div>
+  </div>
+
+  <RouterView />
+</template>
+<style scoped>
+#navbar {
+  max-width: 1290px;
+  margin: auto;
+}
+
+#nav a :hover {
+  color: yellow;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+<template>
+  <div id="HeroArea">
+    <div id="mainHeader" class="bg-mainBanner h-full w-full">
+      <div id="heroSection" class="h-full">hello</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+#HeroArea {
+  height: 650px;
+}
+#mainHeader {
+  background-position: 50%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+}
+</style>
